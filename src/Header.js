ef5272b1d2090ccf0ee772c849de11f75414a9a1
Zachary Babers <zacharybabers@gmail.com>
2023-02-05 01:38:53 -0500

    Finished home page top bar.
    It's 100% responsive!!! :D

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Nameplate from './Nameplate';
 import Socials from './Socials';
 import Headtabs from './Headtabs';
+import MobileNav from './MobileNav';
 
 const Header = (props) => {
     return(
         <div className='mt-4'>
+            <MobileNav/>
             <Socials />
             <Nameplate/>
             <Headtabs/>
--- src/Nameplate.js
+++ src/Nameplate.js
@@ -1,20 +1,21 @@
 import React from "react";
 import menuburger from "./media/images/Misc/menu-burger.svg"
 
+
 const Nameplate = () => {
     return (
         <div className="flex justify-around font-inter max-sm:text-3xl sm:text-3xl md:text-5xl lg:text-5xl text-center font-semibold">
-             <button className="md:hidden">
-                <img src={menuburger} className="" alt='menu burger' />
-            </button>
             <h1>
                 Zach Babers™
             </h1>
-            <div className="md:hidden">
-
-            </div>
         </div>
     );
 }
 
 export default Nameplate;
+
+/*
+<button className="md:hidden">
+                <img src={menuburger} className="" alt='menu burger' />
+            </button>
+*/
