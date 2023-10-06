d99457eb30390e8824307cadd26faa6747fd947f
Zachary Babers <zacharybabers@gmail.com>
2023-10-06 01:45:26 -0500

    start on about page

--- src/Aboutpage.js
+++ src/Aboutpage.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Aboutpage = () => {
     return (
-        <div>
-            About
+        <div className='m-10 mx-18 flex items-center flex-col space-y-5'>
+            <div className='md:w-9/12'>
+                <div className='font-inter font-bold text-6xl'>
+                    Hi, I'm Zach.
+                </div>
+            </div>
         </div>
     );
 }
