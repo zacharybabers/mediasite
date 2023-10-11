d3f2cc303817f363ab72d5738ccadd93f548036b
Zachary Babers <zacharybabers@gmail.com>
2023-10-11 16:55:39 -0500

    added new icon, also added initial object markup for code project objects

---
+++ src/codecomponents/CodeProjects.js
@@ -0,0 +1,22 @@
+export const CodeProjects = [
+    {
+        id: 1,
+        contentType: 'code',
+        title: '3D Voxel Engine',
+        description: 'Voxel Engine developed in Unity 3D.',
+        pic1Path: 'contentsource/photos/render.png',
+        pic2Path: 'contentsource/photos/otherrender.png',
+        tags: ['C#', 'Unity', 'Fastnoise', 'Git'],
+        githubLink: 'https://github.com/zacharybabers/voxel-experimentation',
+    },
+    {
+        id: 2,
+        contentType: 'code',
+        title: 'Pirate Defense',
+        description: 'Tower Defense game developed in Unity 3D.',
+        pic1Path: 'contentsource/photos/render.png',
+        pic2Path: 'contentsource/photos/otherrender.png',
+        tags: ['C#', 'Unity', 'SourceTree', 'Git'],
+        githubLink: 'https://github.com/zacharybabers/Pirate-Defense',
+    },
+]
