0f43551b2941c988a24cf7ead1ffdf52e7f4c86a
Zachary Babers <zacharybabers@gmail.com>
2023-10-16 22:26:31 -0500

    change name of page on index, edit about page layout a little bit

--- src/Aboutpage.js
+++ src/Aboutpage.js
@@ -5,22 +5,24 @@ const Aboutpage = () => {
         <div className='m-10 mx-18 flex items-center flex-col space-y-5'>
             <div className='md:w-9/12'>
                 <div className='font-inter font-bold text-6xl'>
-                    Hi, I'm Zach.
-                </div>
-                <div className='font-ibm font-bold text-2xl mt-6'>
-                    CS Student, Game Developer, Writer
+                            Hi, I'm Zach.
                 </div>
             </div>
             <div className='md:w-9/12 flex flex-col md:flex-row md:justify-between'>
-                <div className='md:w-3/12'>
-                    <div>
-                        Studying software development and computer science at Washington University in St. Louis, I have an interest in webdev, gamedev, procedural generation, and learning more about different ways to apply programming in general. I also love writing, fashion, running, and animation.
+                <div>
+                    <div className='font-ibm font-bold text-2xl mt-6'>
+                        CS Student, Game Developer, Writer
                     </div>
-                    <div className='mt-6'>
-                        Contact: zacharybabers@gmail.com
+                    <div className='md:w-4/12 mt-6'>
+                        <div>
+                            Studying software development and computer science at Washington University in St. Louis, I have an interest in webdev, gamedev, procedural generation, and learning more about different ways to apply programming in general. I also love writing, fashion, running, and animation.
+                        </div>
+                        <div className='mt-6'>
+                            Contact: zacharybabers@gmail.com
+                        </div>
                     </div>
                 </div>
-                <img src='contentsource/photos/faceshot.png' alt='me' className="object-cover h-30 md:h-full md:w-3/12 shrink shadow-md shadow-stone-800 outline-2 mt-4 md:mt-0 mr-40 rounded-md" />
+                <img src='contentsource/photos/faceshot.png' alt='me' className="object-cover h-30 md:h-full md:w-3/12 shrink shadow-md shadow-stone-800 outline-2 mt-6 md:mt-6 mr-40 rounded-md" />
             </div>
         </div>
     );
