e00a3359f6aa29d628caf19d8d784844d2d11100
Zachary Babers <zacharybabers@gmail.com>
2023-10-23 12:52:39 -0500

    add 3D projects

--- src/Mediapage.js
+++ src/Mediapage.js
@@ -7,7 +7,7 @@ const Mediapage = () => {
         <div>
             <div className='m-10 mx-18 flex justify-center items-center flex-col space-y-5'>
                     <div className="font-inter font-semibold">
-                        welcome 👋🏾 click items below to start exploring things I've worked on, or go to one of the pages above to see a specific type of content.
+                        welcome 👋🏾 click items below to start exploring my stuff, or go to one of the pages above to see a specific type of content.
                     </div>
             </div>
             <div>
