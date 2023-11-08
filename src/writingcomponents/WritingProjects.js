b4bea6baa1d99d235806f68713c8d1834006f132
Zachary Babers <zacharybabers@gmail.com>
2023-11-08 06:07:33 -0600

    added writings that were on tumblr, todo add new writings then be done

--- src/writingcomponents/WritingProjects.js
+++ src/writingcomponents/WritingProjects.js
@@ -1,5 +1,7 @@
 import GenericWritingPage from "./GenericWritingPage"
 import TearsInRain from "./writingpages/TearsInRain"
+import TheBetrayal from "./writingpages/TheBetrayal"
+import TheDeep from "./writingpages/TheDeep"
 
 export const WritingProjects = [
     {
@@ -10,31 +12,33 @@ export const WritingProjects = [
         synopsis: "Amid the endless rain, a city stands as humanity's last refuge. But when Rex, a seemingly ordinary duplicate, stumbles upon a mysterious holotape, it unlocks secrets that threaten to upend everything.",
         writingType: 'short story',
         genre: 'sci-fi',
-        date: '9.11.22',
+        date: '11.15.21',
         route: '/tearsinrain',
         writingPage: <TearsInRain/>
     },
     {
         id: 2,
         contentType: 'writing',
-        title: 'Tears In Rain',
-        teaser: 'When the rain began, humanity rejoiced. Earth, its surface dried and cracked from decades of drought, was once again made to cry; the drizzle that began as a small test of Terra I, the terraforming machine intended to bring humanity beyond Earth, swiftly snowballed in intensity, developing into an unending torrent. As days of storm turned into weeks, months, and years, a flood unlike any other ravaged the planet. In a final, desperate effort to preserve their way of life, a new technological renaissance known as the Last Flourish began; mankind searched for a way to survive the endless downpour. Along with numerous other technological advancements, so too came the floating cities: humanity’s last ark on Earth’s now endless and ever deepening ocean. In 4400 AD, the city of Meridian lives on...',
-        synopsis: "Amid the endless rain, a city stands as humanity's last refuge. But when Rex, a seemingly ordinary duplicate, stumbles upon a mysterious holotape, it unlocks secrets that threaten to upend everything.",
+        title: 'The Betrayal',
+        teaser: '',
+        synopsis: "Swashbuckling, mutiny, and lust for power and gold; this pirate story was borne of a deep love for the classic tales that captured my imagination and set my heart on fire as a kid.",
         writingType: 'short story',
-        genre: 'sci-fi',
-        date: '10.13.22',
-        route: '/tearsinrain',
-        writingPage: <GenericWritingPage/>
+        genre: 'swashbuckler',
+        date: '05.31.19',
+        route: '/thebetrayal',
+        writingPage: <TheBetrayal/>
     },
     {
         id: 3,
         contentType: 'writing',
-        title: 'Tears In Rain',
-        teaser: 'When the rain began, humanity rejoiced. Earth, its surface dried and cracked from decades of drought, was once again made to cry; the drizzle that began as a small test of Terra I, the terraforming machine intended to bring humanity beyond Earth, swiftly snowballed in intensity, developing into an unending torrent. As days of storm turned into weeks, months, and years, a flood unlike any other ravaged the planet. In a final, desperate effort to preserve their way of life, a new technological renaissance known as the Last Flourish began; mankind searched for a way to survive the endless downpour. Along with numerous other technological advancements, so too came the floating cities: humanity’s last ark on Earth’s now endless and ever deepening ocean. In 4400 AD, the city of Meridian lives on...',
-        synopsis: 'The story of how a man overcame his nature and became something more than he could ever imagine. Watch as ties are cut, demons are slain, and life is created. Idk',
+        title: 'The Deep',
+        teaser: 'Dunmoor was an old city. Its layered structure made this evident. Beneath bustling districts of elegant cathedrals and coal factories was the old city, rendered little more than a wooden ruin by the unyielding push of progress. Still further down were the depths, dark recesses untouched by the city’s citizens, spoken only of by parents eager to strike fear into their children. Dunmoor was also a grey city. Even in autumn, the bright hues of red and yellow leaves were nowhere to be seen. The smog-filled sky seemed to choke out the sun, leaving the streets shrouded in a seemingly eternal dusk. These streets were filled with roaming citizens of all ages, and they too were grey. Their skin was pale from long days spent away from the light of the sun, and any color that had been in their garments was blotted out by layers of soot...',
+        synopsis: 'Lady Eleanor Austere is a member of the elite of the industrious city of Dunmoor. Attending an enigmatic masquerade party, Eleanor becomes entangled in a web of secrets and sinister forces.',
         writingType: 'short story',
-        genre: 'sci-fi',
-        date: '10.13.22',
+        genre: 'dark fantasy',
+        date: '10.06.21',
+        route: '/thedeep',
+        writingPage: <TheDeep/>
     },
     {
         id: 4,
