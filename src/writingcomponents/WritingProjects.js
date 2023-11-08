3395154830cba9cd66435f237f9628aa6305d332
Zachary Babers <zacharybabers@gmail.com>
2023-11-07 18:05:32 -0600

    added tearsinrain story, first writing added :)

--- src/writingcomponents/WritingProjects.js
+++ src/writingcomponents/WritingProjects.js
@@ -1,4 +1,5 @@
 import GenericWritingPage from "./GenericWritingPage"
+import TearsInRain from "./writingpages/TearsInRain"
 
 export const WritingProjects = [
     {
@@ -6,23 +7,23 @@ export const WritingProjects = [
         contentType: 'writing',
         title: 'Tears In Rain',
         teaser: 'When the rain began, humanity rejoiced. Earth, its surface dried and cracked from decades of drought, was once again made to cry; the drizzle that began as a small test of Terra I, the terraforming machine intended to bring humanity beyond Earth, swiftly snowballed in intensity, developing into an unending torrent. As days of storm turned into weeks, months, and years, a flood unlike any other ravaged the planet. In a final, desperate effort to preserve their way of life, a new technological renaissance known as the Last Flourish began; mankind searched for a way to survive the endless downpour. Along with numerous other technological advancements, so too came the floating cities: humanity’s last ark on Earth’s now endless and ever deepening ocean. In 4400 AD, the city of Meridian lives on...',
-        synopsis: 'The story of how a man overcame his nature and became something more than he could ever imagine. Watch as ties are cut, demons are slain, and life is created. Idk',
+        synopsis: "Amid the endless rain, a city stands as humanity's last refuge. But when Rex, a seemingly ordinary duplicate, stumbles upon a mysterious holotape, it unlocks secrets that threaten to upend everything.",
         writingType: 'short story',
         genre: 'sci-fi',
-        date: '10.13.22',
+        date: '9.11.22',
         route: '/tearsinrain',
-        writingPage: <GenericWritingPage/>
+        writingPage: <TearsInRain/>
     },
     {
         id: 2,
         contentType: 'writing',
-        title: 'The Invitation',
+        title: 'Tears In Rain',
         teaser: 'When the rain began, humanity rejoiced. Earth, its surface dried and cracked from decades of drought, was once again made to cry; the drizzle that began as a small test of Terra I, the terraforming machine intended to bring humanity beyond Earth, swiftly snowballed in intensity, developing into an unending torrent. As days of storm turned into weeks, months, and years, a flood unlike any other ravaged the planet. In a final, desperate effort to preserve their way of life, a new technological renaissance known as the Last Flourish began; mankind searched for a way to survive the endless downpour. Along with numerous other technological advancements, so too came the floating cities: humanity’s last ark on Earth’s now endless and ever deepening ocean. In 4400 AD, the city of Meridian lives on...',
-        synopsis: 'Be careful what you wish for... this person received a crazy letter in the mail that changed their life, ruptured their friendships, and caused huge rifts in their personal relationships.',
-        writingType: 'poem',
-        genre: 'dark fantasy',
-        date: '9.22.21',
-        route: '/theinvitation',
+        synopsis: "Amid the endless rain, a city stands as humanity's last refuge. But when Rex, a seemingly ordinary duplicate, stumbles upon a mysterious holotape, it unlocks secrets that threaten to upend everything.",
+        writingType: 'short story',
+        genre: 'sci-fi',
+        date: '10.13.22',
+        route: '/tearsinrain',
         writingPage: <GenericWritingPage/>
     },
     {
