3a9571593969b57e51d6390ee27496a83de1b3eb
Zachary Babers <zacharybabers@gmail.com>
2023-11-12 04:37:38 -0600

    added photos, photo albums, descriptions, layouts, etc

--- src/PhotoProjects.js
+++ src/PhotoProjects.js
@@ -2,19 +2,64 @@ export const PhotoProjects = [
     {
         id: 1,
         contentType: 'photo',
-        title: 'MyCol',
-        description: 'Sample images being used for website mockup.',
-        date: "12.27.22",
-        imageSources: ['contentsource/photos/squarepic.png', 'contentsource/photos/busch.png', 'contentsource/gifs/horizontalgif.gif', 'contentsource/photos/faceshot.png'],
-        layouts: ['square', 'vertical', 'horizontal', 'none']
+        title: "Halloween '23",
+        description: 'I was a knight for halloween. Gotta be a top 3 costume for me.',
+        date: "10.29.23",
+        imageSources: ['contentsource/PhotoFolders/Halloween23/Halloween1.JPG', 'contentsource/PhotoFolders/Halloween23/Halloween2.JPG', 'contentsource/PhotoFolders/Halloween23/Halloween3.JPEG'],
+        layouts: ['vertical', 'none', 'none']
     },
     {
         id: 2,
         contentType: 'photo',
-        title: 'OtherCol',
-        description: 'Sample images being used for website mockup.',
-        date: "12.27.22",
-        imageSources: ['contentsource/photos/squarepic.png', 'contentsource/photos/busch.png', 'contentsource/gifs/horizontalgif.gif', 'contentsource/photos/faceshot.png'],
-        layouts: ['square', 'vertical', 'horizontal', 'none']
-    }
+        title: 'Summer @ Home',
+        description: "My time at home during summer '23. Wish it could have been longer tbh. ",
+        date: "8.12.23",
+        imageSources: ['contentsource/PhotoFolders/SummerHome/Summer1.JPG', 'contentsource/PhotoFolders/SummerHome/Summer2.JPG', 'contentsource/PhotoFolders/SummerHome/Summer3.jpg', 'contentsource/PhotoFolders/SummerHome/Summer4.png', 'contentsource/PhotoFolders/SummerHome/Summer5.png'],
+        layouts: ['vertical', 'none', 'none', 'horizontal', 'none']
+    },
+    {
+        id: 3,
+        contentType: 'photo',
+        title: 'Summer Internship',
+        description: "Scenes from my first internship at Microsoft in St. Louis.",
+        date: "5.25.23",
+        imageSources: ['contentsource/PhotoFolders/InternshipStuff/Internship1.JPG', 'contentsource/PhotoFolders/InternshipStuff/Internship2.png', 'contentsource/PhotoFolders/InternshipStuff/Internship3.png'],
+        layouts: ['none', 'none', 'vertical']
+    },
+    {
+        id: 4,
+        contentType: 'photo',
+        title: 'Break in Boston',
+        description: "Visited Boston for spring break. Not many pics tbh; it may be so cold that cameras don't work there.",
+        date: "3.16.23",
+        imageSources: ['contentsource/PhotoFolders/Boston/Boston1.png', 'contentsource/PhotoFolders/Boston/Boston2.png'],
+        layouts: ['none', 'none']
+    },
+    {
+        id: 5,
+        contentType: 'photo',
+        title: 'Winter Cabin',
+        description: "I'm not gon say too much. Just go head n get in that tub.",
+        date: "1.5.23",
+        imageSources: ['contentsource/PhotoFolders/WinterCabin/Cabin1.png', 'contentsource/PhotoFolders/WinterCabin/Cabin2.png', 'contentsource/PhotoFolders/WinterCabin/Cabin3.JPG'],
+        layouts: ['square', 'horizontal', 'none']
+    },
+    {
+        id: 6,
+        contentType: 'photo',
+        title: 'Suited Shoot',
+        description: "I MIGHT be a devil hunter. Credits to Jason Wood for the pictures.",
+        date: "12.30.23",
+        imageSources: ['contentsource/PhotoFolders/SuitedShoot/Shoot1.png', 'contentsource/PhotoFolders/SuitedShoot/Shoot2.jpg', 'contentsource/PhotoFolders/SuitedShoot/Shoot3.PNG', 'contentsource/PhotoFolders/SuitedShoot/Shoot4.jpg'],
+        layouts: ['square', 'none', 'none', 'horizontal']
+    },
+    {
+        id: 7,
+        contentType: 'photo',
+        title: 'Busch Gardens',
+        description: "Don't go here unless you can handle the power of sum Roman Gods. RIP my old phone.",
+        date: "7.21.22",
+        imageSources: ['contentsource/PhotoFolders/Busch/Busch1.png', 'contentsource/PhotoFolders/Busch/Busch2.png'],
+        layouts: ['vertical', 'none']
+    },
 ]
